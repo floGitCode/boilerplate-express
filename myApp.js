a22c587d982e87bc977472756c40fafff7dacd41
floGitCode <94071293+floGitCode@users.noreply.github.com>
2022-01-17 16:33:10 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var fs = require('fs');
+
+var absolutePath = path.join(__dirname, '/views/index.html');
+var dirPublic = path.join(__dirname, '/public')
+/*console.log("Hello World");*/
+app.get((req, res) => {
+    res.send("Hello World");
+});
+
+app.get('/', (req, res) => {
+    res.sendfile(absolutePath)
+});
+
+app.use('/public', express.static(dirPublic))
 
 
 
